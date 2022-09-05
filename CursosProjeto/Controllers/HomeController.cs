3d0e59c3f5cbd4e76908e4a726f7349b6bb00318
FagnerMoraes <fagm.carvalho@gmail.com>
2022-09-05 16:42:26 -0300

    modificando view curso

--- CursosProjeto/Controllers/HomeController.cs
+++ CursosProjeto/Controllers/HomeController.cs
@@ -1,21 +1,29 @@
-﻿using CursosProjeto.Models;
+﻿using CursosProjeto.Data;
+using CursosProjeto.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CursosProjeto.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+      
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return _context.cursos != null ?
+                          View(await _context.cursos.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.cursos'  is null.");
         }
 
         public IActionResult Privacy()
